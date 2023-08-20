24e28c8abdd8cece5fe297e5c7f63b26e5ad9230
Julio Corradini <julcorradi@gmail.com>
2023-08-20 05:00:31 -0300

    agregando los arvhivos app.js, index.html y styles.css. Falta arreglar la conexion a la base de datos.

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = 3000;
+
+// Configuración de la base de datos MySQL
+const db = mysql.createConnection({
+  host: 'MySQL 8.1 Serve', // Base de datos: julio_loign_1 / Nombre de usuario: julio_login_1 / Correo electrónico: julcorradi@gmail.com
+  user: 'julio_login_1@localhost',
+  password: 'juliologin',
+  database: 'julio_login_1'
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Ruta de inicio
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta de registro de usuario
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  db.query(
+    'INSERT INTO users (username, password) VALUES (?, ?)',
+    [username, hashedPassword],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.redirect('/');
+      } else {
+        res.redirect('/');
+      }
+    }
+  );
+});
+
+// Ruta de inicio de sesión
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  db.query(
+    'SELECT * FROM users WHERE username = ?',
+    [username],
+    async (err, result) => {
+      if (err || result.length === 0) {
+        res.redirect('/');
+      } else {
+        const isPasswordValid = await bcrypt.compare(password, result[0].password);
+        if (isPasswordValid) {
+          req.session.user = result[0];
+          res.redirect('/dashboard');
+        } else {
+          res.redirect('/');
+        }
+      }
+    }
+  );
+});
+
+// Ruta del panel de control después del inicio de sesión exitoso
+app.get('/dashboard', (req, res) => {
+  if (req.session.user) {
+    res.send(`¡Hola, ${req.session.user.username}! Has iniciado sesión correctamente.`);
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
+});
