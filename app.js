2339b9f57474c390bbb8f40c44665aace22270ad
Julio Corradini <julcorradi@gmail.com>
2023-08-21 12:55:13 -0300

    Arreglando los estilos de la app. Descargando el módulo express-validator para mejorar la seguridad de la app y poder validar lo que se ingresa en los campos. Hay que arreglar la funcionalidad de express-validator y agregar otras medidas de seguridad.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql2');
+const { body, validationResult } = require('express-validator');
 
 const app = express();
 const PORT = 3000;
@@ -24,6 +25,7 @@ app.use(
     saveUninitialized: true
   })
 );
+app.use(express.static(__dirname));
 
 // Ruta de inicio
 app.get('/', (req, res) => {
@@ -50,7 +52,10 @@ app.post('/register', async (req, res) => {
 });
 
 // Ruta de inicio de sesión
-app.post('/login', (req, res) => {
+app.post('/login', [
+  body('username').notEmpty().withMessage('El nombre de usuario es obligatorio'), // Esta funcionalidad de validación todavía no funciona bien
+  body('password').notEmpty().withMessage('La contraseña es obligatoria')
+], (req, res) => {
   const { username, password } = req.body;
 
   db.query(
@@ -63,7 +68,7 @@ app.post('/login', (req, res) => {
         const isPasswordValid = await bcrypt.compare(password, result[0].password);
         if (isPasswordValid) {
           req.session.user = result[0];
-          res.redirect('/dashboard');
+          res.redirect('/dashboard'); // Aquí la acción tiene que ser la de seguir el link que redirige al portal con las historias clínicas.
         } else {
           res.redirect('/');
         }
@@ -72,7 +77,7 @@ app.post('/login', (req, res) => {
   );
 });
 
-// Ruta del panel de control después del inicio de sesión exitoso
+// Ruta del panel de control después del inicio de sesión exitoso. En realidad aqui se debe seguir el link al portal con las historias clínicas.
 app.get('/dashboard', (req, res) => {
   if (req.session.user) {
     res.send(`¡Hola, ${req.session.user.username}! Has iniciado sesión correctamente.`);
