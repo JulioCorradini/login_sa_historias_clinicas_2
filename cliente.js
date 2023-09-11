9556a9602e4eacc97d81064dd4c47924a648e79e
Julio Corradini <julcorradi@gmail.com>
2023-09-11 13:25:18 -0300

    Arreglando los estilos de los mensajes de error y los div ocultos.

--- cliente.js
+++ cliente.js
@@ -8,7 +8,7 @@ function validarContraseña(password) {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$/;
   //return password.match(passwordRegex) !== null;
   if(!password.match(passwordRegex)){
-    textoDeError.innerText = "El campo no puede quedar en blanco. La contraseña debe tener al menos 8 caracteres, al menos una minúscula, al menos una mayúscula, al menos un dígito y al menos un caracter especial";
+    textoDeError.innerText = "El campo no puede quedar en blanco. La contraseña debe tener al menos 8 caracteres, al menos una minúscula, al menos una mayúscula, al menos un dígito y al menos un caracter especial.";
     mensajeDeError.appendChild(textoDeError);
     container.style.display = "none";
     containerOculto.style.display = "block";
