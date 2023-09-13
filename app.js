31f38234c9e1966fa4c6092c218b574a9a94ea19
Julio Corradini <julcorradi@gmail.com>
2023-09-13 12:43:55 -0300

    Cmabaindo la base de datos a una nueva en freeslqdatabase.com.

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ var destinatario = ''; // Esta variable queda vacía hasta que se aaceda ala rut
 // Configuración de la base de datos MySQL
 const db = mysql.createConnection({
   // Variables de entorno con las credenciales de una Base de Datos de prueba en mi localhost
-  host: process.env.DB_HOST_LOCAL,
+  /*host: process.env.DB_HOST_LOCAL,
   user: process.env.DB_USER_LOCAL,
   password: process.env.DB_PASS_LOCAL,
-  database: process.env.DB_NAME_LOCAL
+  database: process.env.DB_NAME_LOCAL*/
 
 
-  // Estas son las credenciales de una Base de Datos de prueba en el servidor de freesqldatabase.com
+  // Estas son las varirables de entorno con las credenciales de una Base de Datos de prueba en el servidor de freesqldatabase.com
   //App deployada para test en el servidor gratuito de render.com. Se accede mediante el link https://login-sa-historias-clinicas-2.onrender.com
-  //La base de datos de freesqldatabase.com se puede visualizar accediendo al link https://www.phpmyadmin.co/ con las correspondientes credenciales.
-  /*host: 'sql10.freesqldatabase.com',
-  user: 'sql10643279',
-  password: 'WM5jknmXy9',
-  database: 'sql10643279'*/
+  //La base de datos de freesqldatabase.com se puede visualizar accediendo al link https://www.phpmyadmin.co/db_structure.php?server=1&db=sql9646191 con las correspondientes credenciales.
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 //Variables de entrorno con las credenciales para enviar el mail con "nodemailer".
