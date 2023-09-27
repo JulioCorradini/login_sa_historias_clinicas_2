151812a324e20e4add1be9d12612ca498d48ac6e
Julio Corradini <julcorradi@gmail.com>
2023-09-27 11:03:47 -0300

    Arreglando el CSS para mejorar los estilos.

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ var destinatario = ''; // Esta variable queda vacía hasta que se acceda a la ru
 // Configuración de la base de datos MySQL
 const db = mysql.createConnection({
   // Variables de entorno con las credenciales de una Base de Datos de prueba en mi localhost
-  host: process.env.DB_HOST_LOCAL,
+  /*host: process.env.DB_HOST_LOCAL,
   user: process.env.DB_USER_LOCAL,
   password: process.env.DB_PASS_LOCAL,
-  database: process.env.DB_NAME_LOCAL
+  database: process.env.DB_NAME_LOCAL*/
 
 
   // Estas son las varirables de entorno con las credenciales de una Base de Datos de prueba en el servidor de freesqldatabase.com
   //App deployada para test en el servidor gratuito de render.com. Se accede mediante el link https://login-sa.onrender.com/
   //La base de datos de freesqldatabase.com se puede visualizar accediendo al link https://www.phpmyadmin.co con las correspondientes credenciales.
-  /*host: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME*/
+  database: process.env.DB_NAME
 });
 
 //Variables de entrorno con las credenciales para enviar el mail con "nodemailer".
@@ -97,7 +97,7 @@ app.post('/register', async (req, res) => {
                     from: EMAIL_USER,// Aquí va el correo oficial del sanatorio
                     to: username,//destinatario, // Aquí debe ir el correo del usuario que se obtiene del parámetro email de la URL
                     subject: 'Registro exitoso',
-                    html: `<p>Hola, su registro está casi completo. Para confirmar el proceso ingrese al siguiente <a href='${serverURL}/indexConfirmacion.html'>link</a> e introduzca el siguiente código de seguridad ${token}. Tiene 5 minútos para realizar este procezo de confirmación, de lo contrario su cuenta será borrada de nuestro sistema y deberá repetir el registro.p</p>`
+                    html: `<p>Hola, su registro está casi completo. Para confirmar el proceso ingrese al siguiente <a href='${serverURL}/indexConfirmacion.html'>link</a> e introduzca el siguiente código de seguridad ${token}. Tiene 5 minútos para realizar este procezo de confirmación, de lo contrario su cuenta será borrada de nuestro sistema y deberá repetir el registro.</p>`
                   }, (error, info) => {
                     if (error) {
                       console.log('Error al enviar el correo electrónico:', error);
