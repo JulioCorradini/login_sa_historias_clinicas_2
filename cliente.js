827d4dd175ec33a35421fcad3798564a98e0056b
Julio Corradini <julcorradi@gmail.com>
2023-10-05 12:46:57 -0300

    Quitando el controlador de regex.js y la ruta que se crea para consumirlo. Ahora, en la parte del cliente, la expresión regular se encuentra hardcodeada en el código cliente.js.

--- cliente.js
+++ cliente.js
@@ -9,19 +9,7 @@ const tituloDelMensaje = document.getElementById("tituloDelMensaje");
 const mensaje = document.getElementById("mensaje");
 const textoDelMensaje = document.getElementById("textoDelMensaje");
 
-var passwordRegex; // Variable que se inicializa vacía y que luego gurdará la respuesta con la expresión regular.
-
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('/regex') // Se realiza una petición GET a la ruta '/regex' para traer la expresión regular y gaurdarla en la variable passwordRegex.
-  .then(response => response.json())
-  .then(data => {
-    regexString = data.regex;
-    passwordRegex = regexString;
-  })
-  .catch(error => {
-    console.error('Error al obtener la expresión regular desde el servidor:', error);
-  });
-});
+passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!()\-_{}[\]:;"'<>,.?/\\|]).{8,}$/;
 
 function validarContraseña(password) {
   if(!password.match(passwordRegex)){
--- controllers/userControllerRegex.js
+++
@@ -1,7 +0,0 @@
-const regex = require('../regex/regex'); // Importa la variable con el regex.
-
-module.exports = {
-    regex: async (req, res) => {
-        res.json({ regex: regex });
-      }
-};
